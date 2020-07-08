819b893651c3a1160ad74f4c9cd58c8535f7a9d0
Анна <shesterikofff@mail.ru>
2020-07-08 11:56:47 +0300

    Создала страницу для просмотра альбома

--- src/Controller/AlbumsController.php
+++ src/Controller/AlbumsController.php
@@ -6,17 +6,31 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AlbumsController extends AbstractController
 {
     /**
      * @Route("/albums", name="albums_list")
+     * * @return Response
      */
     public function showAlbums()
     {
         return $this->render('albums.html.twig', [
             'title' => 'Список альбомов'
+        ]);
+    }
+
+    /**
+     * @Route("/album/view", name="view_album")
+     * @return Response
+     */
+
+    public function viewAlbum()
+    {
+        return $this->render('album/view.html.twig', [
+            'title' => 'Просмотр альбома'
         ]);
     }
 
